2786adf450fbf4e3b03719b1b8ba0c9a68c761f7
Cookie <kwh3884858@gamil.com>
2018-12-06 22:52:47 +0800

    Initial Commit

---
+++ StoryRoom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StoryRoom
+//
+//  Created by 威化饼干 on 6/12/2018.
+//  Copyright © 2018 威化饼干. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
